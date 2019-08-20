a8ef879679c0570d6514515f2fa6725317fad0e6
THIAGO DE BONIS <thiagodebonis@icloud.com>
2019-08-20 01:18:36 -0300

    Correction in Postlist and Home Components

--- src/components/PostList/index.js
+++ src/components/PostList/index.js
@@ -1,43 +1,74 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
 import Container from './styles';
 import { Post } from 'components';
 
-function PostList({ data }) {
-    return (
-        <Container>
-            {data.map(({ post, postCommentsData }) => {
-                return (
-                    <Post
-                        key={post.username}
-                        post={post}
-                        postCommentsData={postCommentsData}
-                    />
-                );
-            })}
-        </Container>
-    );
-}
+import FirstUserAvatar from 'assets/images/avatars/thiagodebonis.jpg';
+import SecondUserAvatar from 'assets/images/avatars/businesswoman.jpg';
+import ThirdUserAvatar from 'assets/images/avatars/stevejobs.jpg';
+import FourthUserAvatar from 'assets/images/avatars/lisabrennanjobs.jpg';
 
-PostList.propTypes = {
-    data: PropTypes.arrayOf(
-        PropTypes.shape({
-            post: PropTypes.shape({
-                date: PropTypes.string.isRequired,
-                message: PropTypes.string.isRequired,
-                userAvatar: PropTypes.string.isRequired,
-                username: PropTypes.string.isRequired,
-            }).isRequired,
-            postCommentsData: PropTypes.arrayOf(
-                PropTypes.shape({
-                    message: PropTypes.string.isRequired,
-                    userAvatar: PropTypes.string.isRequired,
-                    username: PropTypes.string.isRequired,
-                }).isRequired
-            ).isRequired,
-        }).isRequired
-    ).isRequired,
-};
+class PostList extends Component {
+    state = {
+        postListData: [
+            {
+                post: {
+                    date: '04 Jun 2019',
+                    message:
+                        'Thank you so much for coming and reviewing my code in this simple ReactJS project! Thank you very much <3',
+                    userAvatar: FirstUserAvatar,
+                    username: 'Thiago De Bonis',
+                },
+                postCommentsData: [
+                    {
+                        message:
+                            'I recognize your value, future and capability, I, your Human Resources Agent, will invest in you.',
+                        userAvatar: SecondUserAvatar,
+                        username: 'RH',
+                    },
+                ],
+            },
+            {
+                post: {
+                    date: '05 Jun 2019',
+                    message:
+                        'To be successful, you must truly love what you do. Otherwise, considering only the rational side, you simply give up. This is what happens to most people.',
+                    userAvatar: ThirdUserAvatar,
+                    username: 'Steve Jobs',
+                },
+                postCommentsData: [
+                    {
+                        message:
+                            "Wise words, I am very much your fan! Unfortunately we had his demise, a brilliant mind that didn't care about the opinions of others.",
+                        userAvatar: FirstUserAvatar,
+                        username: 'Thiago De bonis',
+                    },
+                    {
+                        message:
+                            'Really @Thiago De Bonis, a brilliant mind, which the world will feel the weight of your loss!',
+                        userAvatar: FourthUserAvatar,
+                        username: 'Lisa Brennan Jobs',
+                    },
+                ],
+            },
+        ],
+    };
+
+    render() {
+        return (
+            <Container>
+                {this.state.postListData.map(({ post, postCommentsData }) => {
+                    return (
+                        <Post
+                            key={post.username}
+                            post={post}
+                            postCommentsData={postCommentsData}
+                        />
+                    );
+                })}
+            </Container>
+        );
+    }
+}
 
 export default PostList;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,9 +5,6 @@ import { LoadingSection, PostSection } from './styles';
 import { LoadingSpinner, Header, PostList } from 'components';
 
 import FirstUserAvatar from 'assets/images/avatars/thiagodebonis.jpg';
-import SecondUserAvatar from 'assets/images/avatars/businesswoman.jpg';
-import ThirdUserAvatar from 'assets/images/avatars/stevejobs.jpg';
-import FourthUserAvatar from 'assets/images/avatars/lisabrennanjobs.jpg';
 
 export default class Home extends Component {
     state = {
@@ -16,48 +13,6 @@ export default class Home extends Component {
             userAvatar: FirstUserAvatar,
             username: 'Thiago De Bonis',
         },
-        postListData: [
-            {
-                post: {
-                    date: '04 Jun 2019',
-                    message:
-                        'Thank you so much for coming and reviewing my code in this simple ReactJS project! Thank you very much <3',
-                    userAvatar: FirstUserAvatar,
-                    username: 'Thiago De Bonis',
-                },
-                postCommentsData: [
-                    {
-                        message:
-                            'I recognize your value, future and capability, I, your Human Resources Agent, will invest in you.',
-                        userAvatar: SecondUserAvatar,
-                        username: 'RH',
-                    },
-                ],
-            },
-            {
-                post: {
-                    date: '05 Jun 2019',
-                    message:
-                        'To be successful, you must truly love what you do. Otherwise, considering only the rational side, you simply give up. This is what happens to most people.',
-                    userAvatar: ThirdUserAvatar,
-                    username: 'Steve Jobs',
-                },
-                postCommentsData: [
-                    {
-                        message:
-                            "Wise words, I am very much your fan! Unfortunately we had his demise, a brilliant mind that didn't care about the opinions of others.",
-                        userAvatar: FirstUserAvatar,
-                        username: 'Thiago De bonis',
-                    },
-                    {
-                        message:
-                            'Really @Thiago De Bonis, a brilliant mind, which the world will feel the weight of your loss!',
-                        userAvatar: FourthUserAvatar,
-                        username: 'Lisa Brennan Jobs',
-                    },
-                ],
-            },
-        ],
     };
 
     componentDidMount() {
@@ -77,7 +32,7 @@ export default class Home extends Component {
     renderPosts = () => {
         return (
             <PostSection>
-                <PostList data={this.state.postListData} />
+                <PostList />
             </PostSection>
         );
     };
